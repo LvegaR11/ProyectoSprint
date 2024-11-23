551788a7af6023112383e61e2c57b1127e92a9c7
LvegR11 <luis.vega970386@gmail.com>
2024-11-22 22:05:56 -0500

    Intercambio de datos

--- src/main/java/unicartagena/web/unidad3/ControladorInicio.java
+++ src/main/java/unicartagena/web/unidad3/ControladorInicio.java
@@ -3,12 +3,19 @@ package unicartagena.web.unidad3;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 @Slf4j 
 public class ControladorInicio {
+    @Value("${index.mensaje}")
+    String datos;
     @GetMapping("/")
-    public String inicio() {
+    public String inicio(Model modelo) {
+        String mesaje = "Saludos desde Sprint MVC";
+        modelo.addAttribute("mesaje", mesaje);
+        modelo.addAttribute("datos", datos);
         log.info("Mostrando el controlador MVC");
         return "index";
     }
